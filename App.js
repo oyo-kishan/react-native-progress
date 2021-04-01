7e40cdc0b1b6e964e63cf6ef5256b247ff42f939
oyo-kishan <kishan.kumar1@oyorooms.com>
2021-04-01 19:56:46 +0530

    basic part completed

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { SafeAreaView,View,Text} from 'react-native';
+import Progress from './components/Progress';
+
+
+export default function App() {
+
+  return (
+    <SafeAreaView style={{flex : 1,justifyContent:'center',alignItems:"center"}}>
+          <Progress
+            percentage={60}
+            width={200}
+            height={200}
+            strokeWidth={20}
+            strokeColor="#27425c"
+            strokeBackgroundColor="#b3cee8"
+            
+          />
+    </SafeAreaView>
+  );
+}
---
+++ components/Progress.js
@@ -0,0 +1,87 @@
+import React, { useRef, useEffect } from 'react';
+import { View, Dimensions, Animated, TextInput } from 'react-native';
+import Svg, { Circle, G } from 'react-native-svg';
+
+
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedInput = Animated.createAnimatedComponent(TextInput);
+
+const Progress = ({ percentage, width, height ,strokeWidth ,strokeColor,strokeBackgroundColor}) => {
+    const stroke = strokeWidth
+    const radius = (Math.min(height,width) - 2 * stroke) / 2
+    const circumference = 2 * Math.PI * radius
+    const origin = radius + stroke;
+
+    const circleRef = useRef();
+    const textRef = useRef();
+    const animated = useRef(new Animated.Value(0)).current
+
+    const animation = (toValue) => {
+        Animated.timing(animated, {
+            toValue,
+            duration: 500,
+            delay: 0,
+            useNativeDriver: true
+        }).start();
+    }
+
+    useEffect(() => {
+        animation(percentage);
+        animated.addListener((animation) => {
+            const strokeDashoffset =circumference- circumference * animation.value/ 100;
+            if (textRef?.current) {
+                textRef.current.setNativeProps({
+                    text: `${Math.round(animation.value)}`
+                })
+            }
+            if (circleRef?.current) {
+                circleRef.current.setNativeProps({
+                    strokeDashoffset
+                })
+            }
+        })
+    }, []);
+
+
+    return (
+        <View style={{ height: (radius+stroke)*2, width : (radius+stroke)*2, justifyContent: 'center', alignItems: 'center' }}>
+            <Svg height="100%" width="100%">
+                <G height="100%" width="100%">
+                    <Circle
+                        cx="50%"
+                        cy="50%"
+                        r={radius}
+                        stroke={strokeBackgroundColor}
+                        strokeWidth={stroke}
+                    />
+
+                    <AnimatedCircle
+                        ref={circleRef}
+                        cx="50%"
+                        cy="50%"
+                        r={radius}
+                        stroke={strokeColor}
+                        strokeWidth={stroke}
+                        strokeDasharray={circumference}
+                        strokeDashoffset={circumference}
+                        transform={`rotate(-90 , ${origin}, ${origin})`}
+
+                    />
+
+                </G>
+            </Svg>
+
+            <AnimatedInput
+                ref={textRef}
+                editable={false}
+                defaultValue="0"
+                underlineColorAndroid={false}
+                style={[
+                    { position: 'absolute', fontSize: 0.75 * radius, color: strokeColor, padding:16, textAlign: "center", fontWeight: 'bold' }]} />
+
+        </View>
+    )
+}
+
+export default Progress;
